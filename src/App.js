dd191b634f818f27e32918ed683f3a5b3d6b3c6f
soffanma28 <soffandluffy@gmail.com>
2022-11-11 21:29:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import TicTacToe from "./components/TicTacToe";
 function App() {
   return (
     <div className="App">
+      <div className='kelompok'>
+        <h3>Tugas Modul 2 - Kelompok 29</h3>
+          <h5>Rizaldy Imam Khadafi - 21120119140119</h5>
+          <h5>Kevin Ryo Pratama - 21120119130098</h5>
+          <h5>Soffan Marsus Ahmad - 21120119130042</h5>
+      </div>
+      <div>
       <TicTacToe />
+      </div>
     </div>
   );
 }
